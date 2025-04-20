3769c7d90d2b9b513d9ef045e6ed14d65c02a624
ducthanh190506 <nguyeenducthanh@gmail.com>
2025-04-20 17:42:06 +0700

    game sdl

--- lol.cpp
+++ lol.cpp
@@ -12,8 +12,8 @@
 
 using namespace std;
 
-const int SCREEN_WIDTH = 1200;
-const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 struct Bullet {
     SDL_Rect rect;
